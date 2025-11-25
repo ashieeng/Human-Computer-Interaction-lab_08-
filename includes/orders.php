dd9b62dcba4b54ba4401d78cd4cdd2907fb36f9e
ashieeng <ashlynbotin@gmail.com>
2025-11-25 14:28:52 +0800

    lab9

---
+++ includes/orders.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'connect.php';
+
+function getAllOrders() {
+    $query = "SELECT invoice.cus_code, customer.cus_fname, customer.cus_initial ,customer.cus_lname, invoice.inv_date, invoice.inv_subtotal, invoice.inv_tax, invoice.inv_total FROM invoice INNER JOIN customer ON invoice.cus_code = customer.cus_code;";
+    $cn = ConnectDB(); // COnnect DB
+    $result = $cn -> query($query); // Executing query
+
+    $data = [];
+    while($row = $result->fetch_assoc()){
+        $data[] = $row;
+    }
+
+    return $data;
+}
---
+++ includes/products.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'connect.php';
+
+function getAllProducts() {
+    $query = "SELECT * FROM product";
+    $cn = ConnectDB(); // COnnect DB
+    $result = $cn -> query($query); // Executing query
+
+    $data = [];
+    while($row = $result->fetch_assoc()){
+        $data[] = $row;
+    }
+
+    return $data;
+}
