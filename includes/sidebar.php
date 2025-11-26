bc3d638cc2d8be9f2cd5c68b34df8d83f40e21c7
ashieeng <ashlynbotin@gmail.com>
2025-11-26 23:32:51 +0800

    updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,16 +16,9 @@
           </li>
           
            <li class="nav-item">
-            <a class="nav-link <?=$page=='Customers'?'active':''?>" href="customers.php">
-              <span data-feather="shopping-cart"></span>
-              Customers
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="customers.html">
-              <span data-feather="shopping-cart"></span>
-              Customers
+            <a class="nav-link <?=$page=='Orders'?'active':''?>" href="orders.php">
+              <span data-feather="heart"></span>
+              Orders
             </a>
           </li>
         </ul>
